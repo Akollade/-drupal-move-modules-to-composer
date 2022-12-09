4654d9b97f48c5325f5acb5f54d850bbfeabc17a
Floran Brutel <fbrutel@akollade.fr>
2022-12-09 11:44:10 +0100

    Handle all cases, next execute commands

--- src/Command/MoveModulesToComposerCommand.php
+++ src/Command/MoveModulesToComposerCommand.php
@@ -9,8 +9,8 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Yaml\Yaml;
-use Symfony\Component\BrowserKit\HttpBrowser;
-use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class MoveModulesToComposerCommand extends Command
 {
@@ -20,7 +20,8 @@ class MoveModulesToComposerCommand extends Command
     protected function configure(): void
     {
         $this
-            ->addArgument('projectPath', InputArgument::OPTIONAL, 'Drupal project path')
+            ->addArgument('project-path', InputArgument::OPTIONAL, 'Drupal project path')
+            ->addOption('site-uri', null, InputOption::VALUE_REQUIRED, 'Site URI if the project is a multisite')
         ;
     }
 
@@ -28,11 +29,19 @@ class MoveModulesToComposerCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
 
-        $projectPath = $input->getArgument('projectPath');
+        $io->title('Move modules to composer');
+
+        $projectPath = $input->getArgument('project-path');
+        $siteUri = $input->getOption('site-uri');
+
         $modulesPath = sprintf('%s/sites/all/modules', $projectPath);
 
-        foreach (glob(sprintf("%s/*", $modulesPath), GLOB_ONLYDIR) as $moduleDirectory) {
+        $modulesDirectories = glob(sprintf("%s/*", $modulesPath), GLOB_ONLYDIR);
+        $io->note(sprintf('Found %d modules', count($modulesDirectories)));
+
+        foreach ($modulesDirectories as $moduleDirectory) {
             $moduleName = basename($moduleDirectory);
+            $io->section(sprintf('Module %s', $moduleName));
 
             $moduleInfo = file_get_contents(sprintf("%s/%s/%s.info.yml", $modulesPath, $moduleName, $moduleName));
 
@@ -40,6 +49,8 @@ class MoveModulesToComposerCommand extends Command
                 $moduleInfoData = Yaml::parse($moduleInfo);
             } catch (ParseException $exception) {
                 printf('Unable to parse the module info YAML: %s', $exception->getMessage());
+
+                return self::FAILURE;
             }
 
             if (false === array_key_exists('version', $moduleInfoData)) {
@@ -47,26 +58,84 @@ class MoveModulesToComposerCommand extends Command
             }
 
             $moduleVersion = $moduleInfoData['version'];
+            $moduleReleasePage = sprintf('https://www.drupal.org/project/%s/releases/%s', $moduleName, $moduleVersion);
+            $moduleComposerVersion = $this->getComposerVersionFromVersion($moduleVersion);
 
-            $io->note(sprintf(
-                "%s - %s => %s",
+            $io->definitionList(
+                ['Release page' => $moduleReleasePage],
+                ['Version' => $moduleVersion],
+                ['Composer version' => $moduleComposerVersion],
+            );
+
+            $isAlreadyInstalledWithComposer = $this->checkIfAlreadyInstalledWithComposer($projectPath, $moduleName);
+            $moduleInfoFromDrush = $this->getModuleInfoFromDrush($projectPath, $moduleName, $siteUri);
+            $moduleNotActivated = 'not installed' === $moduleInfoFromDrush['status'];
+
+            if ($isAlreadyInstalledWithComposer) {
+                $io->warning(sprintf('The module is already required with Composer (version %s)', $moduleInfoFromDrush['version']));
+            }
+
+            if ($moduleNotActivated) {
+                $io->warning('The module is not activated, you can remove it');
+            }
+
+            $io->comment(sprintf(
+                'Command to install the module with composer: composer require drupal/%s:%s',
                 $moduleName,
-                $moduleVersion,
-                $this->getComposerConstraintFromVersion($moduleVersion)
+                $moduleComposerVersion
             ));
-
         }
-
 
         return Command::SUCCESS;
     }
 
-    private function getComposerConstraintFromVersion(string $version): string
+    private function getComposerVersionFromVersion(string $version): string
     {
         if (str_contains($version, '8.x-')) {
-            return str_replace('8.x-', '^', $version);
+            $composerVersion = str_replace('8.x-', '^', $version);
+
+            if (preg_match("/-(alpha|beta)\d+/", $composerVersion, $matches)) {
+                $composerVersion = str_replace($matches[0], "@" . $matches[1], $composerVersion);
+            }
+
+            return $composerVersion;
         }
 
         return $version;
+    }
+
+    private function checkIfAlreadyInstalledWithComposer(string $projectPath, string $moduleName): bool
+    {
+        $composerJson = file_get_contents(sprintf('%s/composer.json', $projectPath));
+        $composerData = json_decode($composerJson, true);
+
+        return array_key_exists('drupal/' . $moduleName, $composerData['require']);
+    }
+
+    private function getModuleInfoFromDrush(string $projectPath, string $moduleName, ?string $siteUri): array
+    {
+        $command = [
+            'drush',
+            'pm-info',
+            $moduleName,
+            '--format=json',
+        ];
+        if ($siteUri) {
+            $command[] = sprintf('--uri=%s', $siteUri);
+        }
+
+        $process = new Process($command, $projectPath);
+
+        try {
+            $process->mustRun();
+        } catch (ProcessFailedException $exception) {
+            if (str_contains($process->getErrorOutput(), 'Command pm-info needs a higher bootstrap level to run')) {
+                throw new \InvalidArgumentException('Your project is a multisite, you must use the option --site-uri=SITE_URI');
+            }
+
+            throw $exception;
+        }
+
+        return json_decode($process->getOutput(), true)[$moduleName];
     }
 }
